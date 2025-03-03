f3a5a64a9b7d91642fde2add8ea2175db8bf163e
Elton Santos <eltonaxl@hotmail.com>
2025-03-03 02:47:28 +0000

    Add Logo on login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import LoginGoogle from "./components/LoginGoogle";
 
 export default function Home() {
   return (
-    <main>
-      <div className="flex justify-center items-center h-screen bg-gray-100 px-5">
-        <div className="bg-green-300 p-12 rounded-lg w-96 max-w-full flex justify-center items-center flex-col gap-2">
-          <LoginForm />
-          <LoginGoogle />
-        </div>
+    <main className="bg-gray-100 h-screen flex flex-col justify-center items-center px-5">
+      <h1 className="font-bold text-4xl mb-5 text-green-600 text-center">Nutr.IA</h1>
+
+      <div className="bg-green-300 p-12 rounded-lg w-96 max-w-full flex justify-center items-center flex-col gap-2">
+        <LoginForm />
+        <LoginGoogle />
       </div>
     </main>
   );
